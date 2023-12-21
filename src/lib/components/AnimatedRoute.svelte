5d537ae24c8cd0fda44ecbf0bd27804934acf63e
Aneesh Sambu <sambuaneesh@gmail.com>
2023-12-21 22:53:20 +0530

    animated login route

---
+++ src/lib/components/AnimatedRoute.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { fly } from "svelte/transition";
+  import { page } from "$app/stores";
+</script>
+
+<!-- key observes for whatever changes in rhs and renders the page -->
+{#key $page.url}
+  <div in:fly={{ y: "-100%", duration: 690 }}>
+    <slot />
+  </div>
+{/key}
--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,30 +1,36 @@
 <script lang="ts">
-    import { page } from "$app/stores";
-  </script>
-  
-  <main class="card w-4/6 bg-neutral text-neutral-content mx-auto my-11 max-w-xl">
+  import { page } from "$app/stores";
+  import AnimatedRoute from "$lib/components/AnimatedRoute.svelte";
+</script>
+
+<AnimatedRoute>
+  <main
+    class="card w-4/6 bg-neutral text-neutral-content mx-auto my-11 max-w-xl"
+  >
     <div class="card-body items-center text-center">
       <slot />
     </div>
   </main>
+</AnimatedRoute>
 
-  <nav class="flex justify-center my-6">
-    <ul class="steps steps-vertical">
-      <a href="/login" class="step step-primary">Sign In</a>
-      <a
-        href="/login/username"
-        class="step"
-        class:step-primary={$page.route.id?.match(/username|photo/g)}>
-        Choose Username
-      </a>
-      <!-- .match(/username|photo/g) = regex for searching that field and applying the class to highlight if on that page -->
-      <a
-        href="/login/photo"
-        class="step"
-        class:step-primary={$page.route.id?.includes("photo")}>
-        Upload Photo
-      </a>
-      <!-- .includes("photo" = alternative to the regex -->
-    </ul>
-  </nav>
-  
+<nav class="flex justify-center my-6">
+  <ul class="steps steps-vertical">
+    <a href="/login" class="step step-primary">Sign In</a>
+    <a
+      href="/login/username"
+      class="step"
+      class:step-primary={$page.route.id?.match(/username|photo/g)}
+    >
+      Choose Username
+    </a>
+    <!-- .match(/username|photo/g) = regex for searching that field and applying the class to highlight if on that page -->
+    <a
+      href="/login/photo"
+      class="step"
+      class:step-primary={$page.route.id?.includes("photo")}
+    >
+      Upload Photo
+    </a>
+    <!-- .includes("photo" = alternative to the regex -->
+  </ul>
+</nav>
