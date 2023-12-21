2bcdfba69913fd08d86c07db56092b7bcb00aeba
Aneesh Sambu <sambuaneesh@gmail.com>
2023-12-21 21:20:38 +0530

    firebase client setup

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfWQESssUfuYFYO6-rItnbh6mtxGva9Js",
+  authDomain: "svelte-tree-2bfdb.firebaseapp.com",
+  projectId: "svelte-tree-2bfdb",
+  storageBucket: "svelte-tree-2bfdb.appspot.com",
+  messagingSenderId: "601679625706",
+  appId: "1:601679625706:web:577733b8ddee2c5fae4693",
+  measurementId: "G-PPG7WL3GLR",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = getAuth();
+export const storage = getStorage();
